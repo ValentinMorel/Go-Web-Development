0e19cb75b6f870c6f41c92c30e3c770652dcf14b
ValentinMorel <valntn.mrl@gmail.com>
2020-08-24 17:56:21 +0200

    Add comments to code.

--- Chapter1-Http_server/tcp_server/main.go
+++ Chapter1-Http_server/tcp_server/main.go
@@ -5,6 +5,7 @@ import (
 	"net"
 )
 
+// put static parameters in cache
 const (
 	ConnType = "tcp"
 	ConnHost = "localhost"
@@ -15,11 +16,15 @@ func main() {
 
 	listener, err := net.Listen(ConnType, ConnHost+":"+ConnPort)
 	if err != nil {
+		// shutdown program
 		log.Fatal(err)
 	}
+	// close the tcp socket when program exits
 	defer listener.Close()
+	// log the active connection
 	log.Printf("Started listening on address : %s and port %s", ConnHost, ConnPort)
 
+	// infinite loop, listening for incoming connections
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
